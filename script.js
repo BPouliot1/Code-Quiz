4abe9f1a35f65820700ce356648cc7076a1e1aaf
Brandon Pouliot <brandonpouliot@gmail.com>
2024-06-01 12:19:20 -0400

    added questions with answer options and the correct answer

---
+++ script.js
@@ -0,0 +1,7 @@
+const questions = [
+    { question: "What does 'var' stand for?", answers: ["Variable", "Variant", "Variety"], correct: 0 },
+    { question: "Which symbol is used for comments in JavaScript?", answers: ["//", "#", "/* */"], correct: 0 },
+    { question: "Commonly used data types DO NOT include:", answers: ["strings", 'booleans', 'alerts', 'numbers'], correct: 2 },
+    { question: "The condition in an if/else statement is enclosed within ___", answers: ['quotes', 'parenthesis', 'curly brackets', 'square brackets'], correct: 1 },
+];
+
